f35338fb7ee174af17d4e8f8def7f116f7daa2c1
Ammar <rohullahcool1999@gmail.com>
2020-03-25 15:00:16 +0500

    JS compressor and decompressor project

--- compress.js
+++ compress.js
@@ -8,25 +8,26 @@ function frequency(str){
             freqs[str[i]] = 1;
         }
   
-}
-}
-console.log(" frequency");    
-
-function sortfreq(freqs){
+     }
+ 
+ }
+ console.log(" frequency");    
+ 
+ function sortfreq(freqs){
     var letters = [];
     for (var ch in freqs){
         letters.push([freqs[ch],ch]);
     }
     return letters.sort();
-
-}
-console.log(sortfreq({'a': 3, 'c': 2, 'b': 1, 'e': 5, 'd': 1, 'g': 1, 'f': 2}));
-
-console.log("  sort frequency");
-
-
-function buildtree(letters){
-
+ 
+ }
+ console.log(sortfreq({'a': 3, 'c': 2, 'b': 1, 'e': 5, 'd': 1, 'g': 1, 'f': 2}));
+ 
+ console.log("  sort frequency");
+ 
+ 
+ function buildtree(letters){
+ 
     while(letters.length>1){
         var leasttwo = letters.slice(0,2);
         var therest = letters.slice(2,letters.length);
@@ -38,28 +39,28 @@ function buildtree(letters){
         console.log(letters);
         letters.sort();
         return letters[0];
-}
-}
-
-console.log("  tree created");
-
-function trimtree( tree)
-{
+ }
+ }
+ 
+ console.log("  tree created");
+ 
+ function trimtree( tree)
+ {
     var p = tree[1];
     if (typeof p === 'string'){
         return p;
-}
+ }
     else
-{
+ {
         return (Array(trimtree(p[0]),trimtree(p[1])));
-}
-}
-node = (trimtree(tree));
-console.log(node);
-console.log("tree trimed");
-
-var codes = {};  
-function assigncodes(node,pat){
+ }
+ }
+ node = (trimtree(tree));
+ console.log(node);
+ console.log("tree trimed");
+ 
+ var codes = {};  
+ function assigncodes(node,pat){
     pat = pat || "";
     if(typeof(node) == typeof("")){
         codes[node] = pat;
@@ -69,24 +70,28 @@ function assigncodes(node,pat){
         assigncodes(node[1],pat+"1");
     }
     return codes;
-}
-console.log(assigncodes(node));
-
-
-console.log("assign code");
-
-
-
-function encode(str){
+ }
+ console.log(assigncodes(node));
+ 
+ 
+ console.log("assign code");
+ 
+ 
+ 
+ function encode(str){
     var output = "";
+  //  frequency(read_string);
     for (var i=0;i<str.length;i++){
         output = output+codes[str[i]];
     }
     return output;
-}
-console.log(" encoded");
-
-function decode(tree,str){
+ }
+ console.log(" encoded");
+ 
+ 
+ 
+ 
+ function decode(tree,str){
     var output="";
     var p=tree;
     for (var i=0;i<str.length;i++){
@@ -103,8 +108,16 @@ function decode(tree,str){
     }
     return  output;
     }
-console.log(decode(trimtree(tree),l));
-
-
-console.log(" decoded");
-    
+ 
+ var fs = require("fs");
+ var read_string = fs.readFileSync("string.txt");
+ console.log(read_string);
+ 
+ //fs.writeFileSync("endoced.txt",encode(read_string))
+ fs.writeFileSync("endoced.txt",frequency(read_string))
+ 
+ 
+ read_string = fs.readFileSync("encoded.txt");
+ 
+ console.log(decode(tree,read_string));
+ fs.writeFileSync("string.txt",decode(tree,read_string));    
