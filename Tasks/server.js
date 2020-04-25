fb7f94c2b77b8a91e16090655e398b427ceb1517
Ammar <rohullahcool1999@gmail.com>
2020-04-25 06:48:54 +0500

    tasks and projects

--- Tasks/server.js
+++ Tasks/server.js
@@ -1,8 +1,41 @@
-var http = require ("http");
-var port= 3000;
-http.createServer(function(req,res){
-   res.writeHead(200,{"Content-Type":"text/plain"});
-   res.end("node js sends get request on 200")
-}).listen(port); 
-console.log("server is hosted on port ",port);
+// var http = require ("http");
+// var port= 3000;
+// http.createServer(function(req,res){
+   // res.writeHead(200,{"Content-Type":"text/plain"});
+   // res.end("node js sends get request on 200")
+// }).listen(port); 
+//console.log("server is hosted on port ");
+var express = require("express");
+var app= express();
+var mysql = require("mysql");
+
+var mysqlConnection=mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password:'',
+    database:'FirstDataBase'
+});
+mysqlConnection.connect(function(error){
+if(!error){
+    console.log("error");
+}else{
+    console.log("Connected");
+}
+});
+app.get("/",function(req,resp){
+   mysqlConnection.query("SELECT * FROM FirstTable   ",function(error,row,fields)
+   {
+    if(!error){
+        console.log("query failed");
+    }else{
+        console.log("qurey succes");
+        console.log(row);
+        resp.send("hello"+row);
+    }
+   }
+
    
+);
+})
+app.listen(1337)
+
