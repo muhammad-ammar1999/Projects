b973741273c33821e32adfff83cbe573645b419b
Ammar <rohullahcool1999@gmail.com>
2020-06-13 12:21:19 +0500

    Data Base Connection

--- Tasks/Server/Server.js
+++ Tasks/Server/Server.js
@@ -1,11 +1,10 @@
-
 var express = require("express");
-var app= express();
+var app = express();
 var mysql = require("mysql");
 var connection = mysql.createConnection({
 host:"localhost",
 user: "root",
-password: "",
+password: "301999",
 database: "articals"
 });
 connection.connect(function(error){
@@ -15,9 +14,22 @@ connection.connect(function(error){
         console.log("connected to DataBase articals");
     }
 });
-connection.query('SELECT * FROM vowels', (err,rows) => {
+connection.query('SELECT id FROM vowels', (err,rows) => {
     if(err) throw err;
   
     console.log('Data received from Db:');
     console.log(rows);
   });
+ app.get("/",function(req,resp){
+    connection.query(" SELECT * FROM vowels" ,function(error,row,fields)
+     {
+      if(error){
+         console.log("query failed");
+     }else{
+         console.log("qurey succes");
+         console.log(row.TOJSON);
+         resp.send("hello"+row.TOJSON);
+     }
+    });
+ })
+ app.listen(1337);
